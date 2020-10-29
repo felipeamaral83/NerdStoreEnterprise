8158ecdfdad1573de6d84013e4751f609c9ee131
Felipe Amaral de Oliveira <felipe.oliveira@invillia.com>
2020-10-29 16:56:02 -0300

    Exibindo os erros para o usuário.

--- src/web/NSE.WebApp.MVC/Controllers/MainController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/MainController.cs
@@ -9,7 +9,12 @@ namespace NSE.WebApp.MVC.Controllers
         protected bool ResponsePossuiErros(ResponseResult resposta)
         {
             if (resposta != null && resposta.Errors.Mensagens.Any())
+            {
+                foreach (var mensagem in resposta.Errors.Mensagens)
+                    ModelState.AddModelError(string.Empty, mensagem);
+
                 return true;
+            }
 
             return false;
         }
