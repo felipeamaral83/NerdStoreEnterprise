80ed375a62491e7da50e76a5ff3f683e4b651a46
Felipe Amaral de Oliveira <felipe.oliveira@invillia.com>
2020-12-01 10:53:38 -0300

    Command e Command Handler.

---
+++ src/building blocks/NSE.Core/Messages/Command.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+using System;
+
+namespace NSE.Core.Messages
+{
+    public abstract class Command : Message
+    {
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+        
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        public virtual bool IsValid()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
