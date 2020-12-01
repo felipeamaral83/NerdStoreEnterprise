b02611ac797bbfff04304db235716f425a92d15a
Felipe Amaral de Oliveira <felipe.oliveira@invillia.com>
2020-12-01 15:22:00 -0300

    Persistência via Command Handler.

--- src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
+++ src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -9,13 +9,30 @@ namespace NSE.Clientes.API.Application.Commands
 {
     public class ClienteCommandHandler : CommandHandler, IRequestHandler<RegistrarClienteCommand, ValidationResult>
     {
+        private readonly IClienteRepository _clienteRepository;
+
+        public ClienteCommandHandler(IClienteRepository clienteRepository)
+        {
+            _clienteRepository = clienteRepository;
+        }
+        
         public async Task<ValidationResult> Handle(RegistrarClienteCommand message, CancellationToken cancellationToken)
         {
             if (!message.IsValid()) return message.ValidationResult;
 
             var cliente = new Cliente(message.Id, message.Nome, message.Email, message.Cpf);
 
-            return message.ValidationResult;
+            var clienteExistente = await _clienteRepository.ObterPorCpf(cliente.Cpf.Numero);
+
+            if (clienteExistente != null)
+            {
+                AdicionarErro("Este CPF já está em uso.");
+                return ValidationResult;
+            }
+
+            _clienteRepository.Adicionar(cliente);
+
+            return await PersistirDados(_clienteRepository.UnitOfWork);
         }
     }
 }
