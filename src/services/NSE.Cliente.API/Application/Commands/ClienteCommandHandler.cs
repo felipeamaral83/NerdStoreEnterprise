719bf48cacbfc2f7f25398791ee71debbf2ec846
Felipe Amaral de Oliveira <felipe.oliveira@invillia.com>
2020-12-01 15:00:12 -0300

    MediatR e Command Handler.

--- src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
+++ src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -1,6 +1,21 @@
-﻿namespace NSE.Clientes.API.Application.Commands
+﻿using FluentValidation.Results;
+using MediatR;
+using NSE.Clientes.API.Models;
+using NSE.Core.Messages;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NSE.Clientes.API.Application.Commands
 {
-    public class ClienteCommandHandler
+    public class ClienteCommandHandler : CommandHandler, IRequestHandler<RegistrarClienteCommand, ValidationResult>
     {
+        public async Task<ValidationResult> Handle(RegistrarClienteCommand message, CancellationToken cancellationToken)
+        {
+            if (!message.IsValid()) return message.ValidationResult;
+
+            var cliente = new Cliente(message.Id, message.Nome, message.Email, message.Cpf);
+
+            return message.ValidationResult;
+        }
     }
 }
