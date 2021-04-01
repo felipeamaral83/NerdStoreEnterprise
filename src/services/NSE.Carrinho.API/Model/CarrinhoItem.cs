ffd2ddc3ceb78025f8a8c361355e65618131b47c
Felipe Amaral de Oliveira <felipe.oliveira-ext@viavarejo.com.br>
2021-04-01 17:23:53 -0300

    Comportamentos de negocio da API - pt I.

--- src/services/NSE.Carrinho.API/Model/CarrinhoItem.cs
+++ src/services/NSE.Carrinho.API/Model/CarrinhoItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 
 namespace NSE.Carrinho.API.Model
 {
@@ -18,5 +19,51 @@ namespace NSE.Carrinho.API.Model
         public Guid CarrinhoId { get; set; }
         
         public CarrinhoCliente CarrinhoCliente { get; set; }
+
+        internal void AssociarCarrinho(Guid carrinhoId)
+        {
+            CarrinhoId = carrinhoId;
+        }
+
+        internal decimal CalcularValor()
+        {
+            return Quantidade * Valor;
+        }
+
+        internal void AdicionarUnidades(int unidades)
+        {
+            Quantidade += unidades;
+        }
+
+        internal bool EhValido()
+        {
+            return new CarrinhoItemValidation().Validate(this).IsValid;
+        }
+
+        public class CarrinhoItemValidation : AbstractValidator<CarrinhoItem>
+        {
+            public CarrinhoItemValidation()
+            {
+                RuleFor(c => c.ProdutoId)
+                    .NotEqual(Guid.Empty)
+                    .WithMessage("Id do produto inválido");
+
+                RuleFor(c => c.Nome)
+                    .NotEmpty()
+                    .WithMessage("O nome do produto não foi informado");
+
+                RuleFor(c => c.Quantidade)
+                    .GreaterThan(0)
+                    .WithMessage("A quantidade miníma de um item é 1");
+
+                RuleFor(c => c.Quantidade)
+                    .LessThan(5)
+                    .WithMessage("A quantidade máxima de um item é 5");
+
+                RuleFor(c => c.Valor)
+                    .GreaterThan(0)
+                    .WithMessage("O valor do item precisa ser maior que 0");
+            }
+        }
     }
 }
