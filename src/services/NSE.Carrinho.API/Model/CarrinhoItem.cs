112b64bd82b0076ad3580385a34241828e5c566a
Felipe Amaral de Oliveira <felipe.oliveira-ext@viavarejo.com.br>
2021-04-01 18:15:42 -0300

    Comportamentos de negocio da API - pt II.

--- src/services/NSE.Carrinho.API/Model/CarrinhoItem.cs
+++ src/services/NSE.Carrinho.API/Model/CarrinhoItem.cs
@@ -35,6 +35,11 @@ namespace NSE.Carrinho.API.Model
             Quantidade += unidades;
         }
 
+        internal void AtualizarUnidades(int unidades)
+        {
+            Quantidade = unidades;
+        }
+
         internal bool EhValido()
         {
             return new CarrinhoItemValidation().Validate(this).IsValid;
